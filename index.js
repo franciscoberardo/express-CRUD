4392f88573f006bdfb09d00427c2fdff816b762f
franciscoberardo <francisco.berardo@speedcast.com>
2024-06-15 13:37:17 -0300

    add cors

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import elementosRoutes from './src/routes/productRoutes.js';
 import errorHandler from './src/middlewares/errorMiddleware.js';
 
 const app = express();
 const port = 3000;
 
+// Configurar CORS para permitir solicitudes desde cualquier origen
+app.use(cors());
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Use "/products"')
-})
+  res.send('Use "/products"');
+});
 
 app.use('/products', elementosRoutes);
 
